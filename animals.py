32ac314913316455e64e628632258b60fbd8168f
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2024-11-02 20:34:39 +0100

    Abstract class removed. This class was not nessesary and would only make it more difficult to program this.

--- animals.py
+++ animals.py
@@ -2,28 +2,9 @@
 This class is responsible for animals entity like sheep and wolf.
 """
 
-from abc import ABC, abstractmethod
 from random import uniform, choice
 
-
-class AnimalBase(ABC):
-    DIRECTIONS: list[tuple[int, int]] = [
-        (1, 0),  # right
-        (-1, 0),  # left
-        (0, 1),  # up
-        (0, -1)  # down
-    ]
-
-    @abstractmethod
-    def move(self) -> None:
-        pass
-
-    @abstractmethod
-    def get_position(self) -> tuple[float, float]:
-        pass
-
-
-class Sheep(AnimalBase):
+class Sheep:
     def __init__(self, init_pos_limit: float = 10.0, move_dist: float = 0.5):
         self.pos_x, self.pos_y = (
             uniform(-abs(init_pos_limit), abs(init_pos_limit))
@@ -32,25 +13,51 @@ class Sheep(AnimalBase):
         self.move_dist = move_dist
 
     def move(self) -> None:
-        direction_x, direction_y = choice(self.DIRECTIONS)
+        directions: list[tuple[float, float]] = [
+            (self.move_dist, 0),   # right
+            (-self.move_dist, 0),  # left
+            (0, self.move_dist),   # up
+            (0, -self.move_dist)   # down
+        ]
 
-        self.pos_x += self.move_dist * direction_x
-        self.pos_y += self.move_dist * direction_y
+        dx, dy = choice(directions)
+        self.pos_x += dx
+        self.pos_y += dy
 
     def get_position(self) -> tuple[float, float]:
         print(f"Sheep x: {self.pos_x}, y: {self.pos_y}")
         return self.pos_x, self.pos_y
 
 
-class Wolf(AnimalBase):
+class Wolf:
     def __init__(self, pos_x: float = 0.0, pos_y: float = 0.0,
                  move_dist: float = 1.0):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.move_dist = move_dist
 
-    def move(self) -> None:
+    def move(self, sheep_list: list[Sheep]) -> None:
+
         pass
+
+    def hunt(self):
+
+
+    def closest_sheep(self, sheep_list: list[Sheep]) -> tuple[Sheep, float]:
+        closest_sheep = None
+        min_euc_dist: float = float("inf")
+
+        for sheep in sheep_list:
+            euc_dist: float = self.euclidean_dist(sheep.pos_x, sheep.pos_y)
+
+            if euc_dist < min_euc_dist:
+                min_euc_dist = euc_dist
+                closest_sheep = sheep
+
+        return closest_sheep, min_euc_dist
+
+    def euclidean_dist(self, sheep_x: float, sheep_y: float) -> float:
+        return ((self.pos_x - sheep_x) ** 2) + ((self.pos_y - sheep_y) ** 2)
 
     def get_position(self) -> tuple[float, float]:
         print(f"Wolf x: {self.pos_x}, y: {self.pos_y}")
